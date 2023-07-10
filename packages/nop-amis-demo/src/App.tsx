f6e5399a4c8a6e5eed02cc48c22a45444820d70a
canonical <canonical_entropy@163.com>
2023-07-10 22:35:17 +0800

    引入样式

--- packages/nop-amis-demo/src/App.tsx
+++ packages/nop-amis-demo/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 import {RenderOptions, RendererProps, render as renderAmis} from 'amis'
 
 import page from './demo.page'
--- packages/nop-amis-demo/src/main.tsx
+++ packages/nop-amis-demo/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/v4-shims.css';
+import 'amis/lib/themes/default.css';
+import 'amis/lib/helper.css';
+import 'amis/sdk/iconfont.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
