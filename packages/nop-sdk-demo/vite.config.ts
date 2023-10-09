6dccfdeed9f6b9c06ab9028ceb132cc9bb707e79
canonical <canonical_entropy@163.com>
2023-10-10 00:00:23 +0800

    增加nop-sdk-demo

---
+++ packages/nop-sdk-demo/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  server: {
+    proxy: {
+      '/r': {
+        target: 'http://localhost:8080/r', // The target URL of the proxy
+        rewrite: (path) => path.replace(/^\/r/, ''), // Rewrite the path if needed
+      },
+      '/graphql': {
+        target: 'http://localhost:8080/graphql', // The target URL of the proxy
+        rewrite: (path) => path.replace(/^\/graphql/, ''), // Rewrite the path if needed
+      },
+      '/q': {
+        target: 'http://localhost:8080/q', // The target URL of the proxy
+        rewrite: (path) => path.replace(/^\/q/, ''), // Rewrite the path if needed
+      },
+      '/f': {
+        target: 'http://localhost:8080/f', // The target URL of the proxy
+        rewrite: (path) => path.replace(/^\/f/, ''), // Rewrite the path if needed
+      },
+    }
+  },
+})
