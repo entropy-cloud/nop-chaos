0ecc5310259f340820f8044442b79465690a81fa
canonical <canonical_entropy@163.com>
2023-10-13 09:13:20 +0800

    更新amis版本到3.4.2

--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -3,10 +3,49 @@ import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import { readFile, writeFile } from 'node:fs/promises';
 
+import fs from 'node:fs'
+import path from 'node:path'
+
 const dtsFiles = [
   "../nop-core/lib/index.d.ts",
   "../nop-amis-vue/lib/index.d.ts"
 ]
+
+
+function copyDirectory(sourceDir, targetDir) {
+  // 确保目标目录存在，如果不存在则创建
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+
+  // 读取源目录中的所有文件和子目录
+  const files = fs.readdirSync(sourceDir);
+
+  // 遍历源目录中的每个文件和子目录
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const targetPath = path.join(targetDir, file);
+
+    // 如果当前项是一个子目录，则递归调用copyDirectory函数
+    if (fs.statSync(sourcePath).isDirectory()) {
+      copyDirectory(sourcePath, targetPath);
+    } else {
+      // 如果当前项是一个文件，则直接拷贝并覆盖目标文件
+      fs.copyFileSync(sourcePath, targetPath);
+    }
+  });
+}
+
+function copyPackageJson(){
+  const json = JSON.parse(fs.readFileSync("./package.json","utf-8"))
+  for(const k in json.dependencies){
+    if(k.startsWith("@nop-chaos")){
+      delete json.dependencies[k]
+    }
+  }
+  fs.writeFileSync('../../nop-sdk/package.json',JSON.stringify(json,null,'  '))
+}
+
 
 async function mergeDts(){
   let content = "";
@@ -25,7 +64,11 @@ export default defineConfig({
       rollupTypes: true,
       outDir: "lib",
       async afterBuild(){
-        return mergeDts()
+        // 将所有的d.ts文件合并到sdk.d.ts中
+        await mergeDts()
+        // 将编译结果拷贝到外部的nop-sdk目录中
+        copyDirectory('./lib','../../nop-sdk/lib')
+        copyPackageJson()
       }
     })
   ],
