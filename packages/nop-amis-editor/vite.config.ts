13c73bec15b56b115f126f0ab2fe0f31cb5af182
canonical <canonical_entropy@163.com>
2023-11-11 18:59:02 +0800

    修改nop-amis-editor的打包规则

--- packages/nop-amis-editor/vite.config.ts
+++ packages/nop-amis-editor/vite.config.ts
@@ -14,18 +14,41 @@ export default defineConfig({
       threshold: 1024
     })
   ],
-  build:{
+  build: {
     rollupOptions: {
       output: {
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
         manualChunks(id) {
-          //console.log("id="+id)
-          if (id.includes('node_modules')) {
-            return "vendor"; 
+          // console.log("id="+id)
+          const libs = ["amis-editor", "monaco-editor", "tinymce", "codemirror",
+            "froala-editor", "exceljs", "xlsx", "office-viewer", "ant-design-vue"];
+          for (let lib of libs) {
+            if (id.includes("node_modules/" + lib + '/'))
+              return lib
           }
-          if (id.includes('/nop-amis-editor/src/')) {
-            return "source"; 
+
+          function include_any(libs) {
+            for (let lib of libs) {
+              if (id.includes("/node_modules/" + lib + '/'))
+                return true
+            }
+            return false
           }
-		  return "app"
+
+
+
+          if (include_any(["echarts", "zrender"]))
+            return "echarts"
+
+          if (include_any(["amis", "amis-ui", "amis-formula", "amis-core", "video-react"]))
+            return "amis";
+
+          if (id.includes('node_modules/@nop-chaos')) {
+            return "nop-sdk";
+          }
+
+
         }
       }
     }
