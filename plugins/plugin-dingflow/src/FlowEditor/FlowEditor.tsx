a753f9d54140e62e83a7cc76fb5c25da26504d9b
canonical <canonical_entropy@163.com>
2024-06-10 20:34:20 +0800

    限制父节点的宽度，从而促使canvas显示横向滚动条

--- plugins/plugin-dingflow/src/FlowEditor/FlowEditor.tsx
+++ plugins/plugin-dingflow/src/FlowEditor/FlowEditor.tsx
@@ -9,6 +9,7 @@ const EditorContainer = styled.div`
   flex: 1;
   display: flex;
   flex-flow: column;
+  width:100%; // 必须设定宽度，否则canvas不会显示滚动条
 `;
 
 export const FlowEditor = memo((props: FlowEditorProps) => {
